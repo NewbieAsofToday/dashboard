9ef1433dceb77fd0d17a9bf3e79232a2eb395146
Azk <azkriven17@gmail.com>
2022-03-11 17:55:42 +0800

    topBar & sidebar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Home from "./pages/home/Home";
+const App = () => {
+  return (
+    <div>
+      <Topbar />
+      <div className='container'>
+        <Sidebar />
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,14 @@
+export const userData = [
+  { name: "Jan", "Active User": 4000 },
+  { name: "Feb", "Active User": 5000 },
+  { name: "March", "Active User": 3000 },
+  { name: "April", "Active User": 5000 },
+  { name: "May", "Active User": 3000 },
+  { name: "June", "Active User": 4000 },
+  { name: "July", "Active User": 2000 },
+  { name: "Aug", "Active User": 1000 },
+  { name: "Sep", "Active User": 2500 },
+  { name: "Oct", "Active User": 3000 },
+  { name: "Nov", "Active User": 2200 },
+  { name: "Dec", "Active User": 2300 },
+];
